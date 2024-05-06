6814675cdefad82b025b34748334f571bf639394
Luca.R04 <Lucarinck04@gmail.com>
2024-05-06 15:27:23 +0200

    Added bullet spawner

--- Gamejam/Assets/Scripts/ProjectileController.cs
+++ Gamejam/Assets/Scripts/ProjectileController.cs
@@ -1,11 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
     public float speed;
+    private Vector2 mousePosition;
+
+    private void Start()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
 
     private void Update()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        // Move the object in the calculated direction
+        transform.Translate(mousePosition * speed * Time.deltaTime);
     }
 }
